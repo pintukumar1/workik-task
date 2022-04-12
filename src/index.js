90320c62cb16c618c617ce68b1957e8a69ac7985
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-04-12 15:43:23 +0530

    header + bootstrap added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
